dbe0bd1d066243d89eef53770077e00067aedd24
active <66892517+activ33@users.noreply.github.com>
2023-09-18 10:16:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "1464463"))
 API_HASH = environ.get("API_HASH", "ff8451462d91861a13ffd8a6bb72aa8b")
-BOT_TOKEN = environ.get("BOT_TOKEN", "6404643240:AAFCTb03FBn34kMVqaagNyj9tkyjGW6G5P0")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6344846447:AAEqnJhFGrZE4auYKHTijHETVsBR6kcJaP0")
 ADMIN = int(environ.get("ADMIN", "689061386"))          
 CAPTION = environ.get("CAPTION", "⚡ Anime Files")
 
